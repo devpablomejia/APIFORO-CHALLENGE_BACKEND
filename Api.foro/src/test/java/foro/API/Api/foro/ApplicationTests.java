4551dcc9c3f71ac99e494bba8d3fe7c0ab06886f
pablo M. Mejía <165319132+devpablomejia@users.noreply.github.com>
2025-07-04 16:27:29 -0500

    project started 🛠️

---
+++ Api.foro/src/test/java/foro/API/Api/foro/ApplicationTests.java
@@ -0,0 +1,13 @@
+package foro.API.Api.foro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
